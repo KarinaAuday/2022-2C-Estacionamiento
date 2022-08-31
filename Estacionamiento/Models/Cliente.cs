e0a194639f17a25ccc373cbff0505277e4d78a42
KARINA AUDAY <karina.auday@ort.edu.ar>
2022-08-31 19:46:58 -0300

    Creo las clases Cliente y Empleado

---
+++ Estacionamiento/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estacionamiento.Models
+{
+    public class Cliente
+    {
+        public int Dni { get; set; }
+
+    }
+}
