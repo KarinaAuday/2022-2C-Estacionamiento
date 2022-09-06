a4afe88ddc02a4226f2b9042d863f841476c2386
KARINA AUDAY <karina.auday@ort.edu.ar>
2022-09-06 18:50:45 -0300

    Agregando las clases

---
+++ Estacionamiento/Models/Persona.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estacionamiento.Models
+{
+    public class Persona
+    {
+         
+        public Persona()
+        {
+
+        }
+
+        public Persona(string nombre, string apellido)
+        {
+            Apellido = apellido;
+            Nombre = nombre;
+
+        }
+
+
+        public int Id { get; set; }
+
+        public Guid Id2 { get; set; }
+
+
+        [Range(1, 99999999, ErrorMessage = "Dni Invalido")]
+        public int Dni { get => dni; set => dni = value; }
+
+        private int dni;
+
+        [StringLength(50, MinimumLength = 2, ErrorMessage = MsjeError.MinMaxString)]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = MsjeError.Requerido)]
+
+        public string Nombre { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        public string NombreCompleto
+        {
+            get
+            {
+                return $"{Apellido}, {Nombre}";
+            }
+        }
+
+       
+        public Direccion Direccion { get; set; }
+
+        public List<Telefono> Telefonos { get; set; }
+
+        
+      
+    }
+}
+
