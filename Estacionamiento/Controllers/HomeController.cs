a1346187afbdeaf1e6c91639344ce76e8ca5fd95
KARINA AUDAY <karina.auday@ort.edu.ar>
2022-09-06 18:17:19 -0300

    Modelo Base

--- Estacionamiento/Controllers/HomeController.cs
+++ Estacionamiento/Controllers/HomeController.cs
@@ -23,21 +23,13 @@ namespace Estacionamiento.Controllers
             return View();
         }
 
-        public IActionResult Index2()
-        {
-            return View();
-        }
+     
 
         public IActionResult Privacy()
         {
             return View();
         }
 
-
-       // public IActionResult Index2()
-        //{
-          //  return View();
-        //}
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
